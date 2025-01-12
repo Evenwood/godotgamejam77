4d3d1c346e1a70bd149462a3811cf0f9eea07803
Evenwood <darienro3ach.37@gmail.com>
2025-01-11 22:52:56 -0500

    Added a base script for statistic resources and then created a collection of statistic resource files using the base statistic gdscript

---
+++ game/StatisticControl/base_statistic.gd
@@ -0,0 +1,10 @@
+# Author: Darien Roach
+# Date Created: 1/11/2025
+# Purpose: The base script to be used for statistic resources to be created from.
+
+extends Resource
+
+# Default values for a statistic
+@export var name:String = "Statistic"
+@export var min_value:int = -10
+@export var max_value:int = 10
