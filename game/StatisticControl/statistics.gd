2cd8eaf43b734b02c60ba17fa4fc7729c1fcf8f1
Evenwood <darienro3ach.37@gmail.com>
2025-01-13 02:49:11 -0500

    Added two new scripts to the project - one specifically for control over the statistics scene and one for maintaining an overarching gamestate

---
+++ game/StatisticControl/statistics.gd
@@ -0,0 +1,15 @@
+# Author: Darien Roach
+# Date Created: 1/13/2025
+# Purpose: The main script for the statistics screen - used to handle processing statistics and their visuals
+
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
