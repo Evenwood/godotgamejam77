5ca3c5828df0d598f4c99193ab84700c06a78cb8
Dieboldcu <76053057+Dieboldcu@users.noreply.github.com>
2025-01-18 15:21:21 -0500

    Add pause menu functionality

--- game/main.gd
+++ game/main.gd
@@ -5,7 +5,11 @@ var paused = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+<<<<<<< Updated upstream
 	pass # Replace with function body.
+=======
+	pass
+>>>>>>> Stashed changes
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
