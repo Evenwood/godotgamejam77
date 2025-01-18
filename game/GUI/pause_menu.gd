2eeb284b130ff95575ff74c7f632f1c5f316a584
Dieboldcu <76053057+Dieboldcu@users.noreply.github.com>
2025-01-18 15:15:00 -0500

    Add pause menu functionality

--- game/GUI/pause_menu.gd
+++ game/GUI/pause_menu.gd
@@ -3,13 +3,38 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$AnimationPlayer.play("RESET")
 
-
+#func resume():
+	#get_tree().paused = false
+	#$AnimationPlayer.play_backwards("blur")
+	#
+#func paused():
+	#get_tree().paused = true
+	#$AnimationPlayer.play("blur")
+#
+#func EscKey():
+	#if Input.is_action_just_pressed("escape") and !get_tree().paused:
+		#paused()
+	#elif Input.is_action_just_pressed("escape") and get_tree().paused:
+		#resume()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+#func _process(delta: float) -> void:
+#	pass
 
-
-func _on_button_3_pressed() -> void:
+func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func _on_restart_pressed() -> void:
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+	get_tree().reload_current_scene()
+
+#func _process(delta):
+
+	
+	#EscKey()
