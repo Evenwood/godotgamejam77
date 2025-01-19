5e36e8b828c817435ead663b6f05c53e37409f15
Evenwood <darienro3ach.37@gmail.com>
2025-01-19 12:02:20 -0500

    Modified the event gui so it shows the events properly

--- game/EventControl/event.gd
+++ game/EventControl/event.gd
@@ -1,15 +1,20 @@
-extends Control
-@onready var event_description: RichTextLabel = $EventCanvas/EventUIContainer/EventDescriptionContainer/EventDescription
-@onready var event_name: Label = $EventCanvas/EventUIContainer/EventName
-@onready var event_image: TextureRect = $EventCanvas/EventUIContainer/EventDescriptionContainer/MarginContainer/EventImage
-@onready var option_1: Button = $EventCanvas/EventUIContainer/ChoiceContainer/VBoxContainer/Option1
-@onready var option_2: Button = $EventCanvas/EventUIContainer/ChoiceContainer/VBoxContainer/Option2
-@onready var option_3: Button = $EventCanvas/EventUIContainer/ChoiceContainer/VBoxContainer/Option3
+extends CanvasLayer
+
+@onready var event_description: RichTextLabel = $EventUIContainer/EventDescriptionContainer/EventDescription
+@onready var event_name: Label = $EventUIContainer/EventName
+@onready var event_image: TextureRect = $EventUIContainer/EventDescriptionContainer/MarginContainer/EventImage
+@onready var option_1: Button = $EventUIContainer/ChoiceContainer/VBoxContainer/Option1
+@onready var option_2: Button = $EventUIContainer/ChoiceContainer/VBoxContainer/Option2
+@onready var option_3: Button = $EventUIContainer/ChoiceContainer/VBoxContainer/Option3
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	event_description.text = EventProcessing.getEventDescription()
+	event_name.text = EventProcessing.getEventName()
+	option_1.text = EventProcessing.getEventOptionOne()
+	option_2.text = EventProcessing.getEventOptionTwo()
+	option_3.text = EventProcessing.getEventOptionThree() 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
